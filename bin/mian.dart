ae89efb78823e4f9b252d5b46f9bda80ddd290ca
유근명 <gmyou71@gmail.com>
2025-04-19 08:14:24 +0900

    Feat : 변수와 제어문
    https://dart-ko.dev/language#%ED%9D%90%EB%A6%84-%EC%A0%9C%EC%96%B4%EB%AC%B8

--- bin/mian.dart
+++ bin/mian.dart
@@ -1,4 +1,36 @@
 // hello world
 void main() {
   print('Hello, world!');
+
+  var name = 'Voyager I';
+  var year = 1977;
+  var antennaDiameter = 3.7;
+  var flybyObjects = ['Jupiter', 'Saturn', 'Uranus', 'Neptune'];
+  var image = {
+    'tags': ['saturn'],
+    'url': '//path/to/saturn.jpg'
+  };
+
+  print(name);
+  print(antennaDiameter);
+  print(image);
+
+  if (year >= 2001) {
+    print('21st century');
+  } else if (year >= 1901) {
+    print('20th century');
+  }
+
+  for (final object in flybyObjects) {
+    print(object);
+  }
+
+  for (int month = 1; month <= 12; month++) {
+    print(month);
+  }
+
+  while (year < 2016) {
+    year += 1;
+  }
+  print(year);
 }
